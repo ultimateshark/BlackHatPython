0c070db895f27e90e44c13dc2f50cb13a7c42e7c
manishpandeycse <manishpandey8858@gmail.com>
2019-03-14 23:34:32 +0530

    tcp connection

---
+++ tcpconnection.py
@@ -0,0 +1,18 @@
+import socket
+
+t_host=input("Enter Host:  \n")
+t_port=int(input("Enter PORT:  \n"))
+
+#creating scocket
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#connect the client
+client.connect((t_host,t_port))
+
+#sending some data
+client.send('GET / HTTP/1.1\r\nHost: '+t_host+'\r\n\r\n')
+
+#receive information
+res=client.recv(4096)
+
+print(res)
